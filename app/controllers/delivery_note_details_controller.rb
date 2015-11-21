3671a00405430041d44c2aed05f7d64a8b79050c
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2015-11-21 20:22:02 +0100

    General improvements / invoice detail.
    Invoice detail creation, and deletion.
    Log level for production environment set to info
    Added bootstrap fonts to assets pipeline

--- app/controllers/delivery_note_details_controller.rb
+++ app/controllers/delivery_note_details_controller.rb
@@ -15,7 +15,7 @@ class DeliveryNoteDetailsController < ApplicationController
 
   # GET /delivery_note_details/new
   def new
-    @delivery_note_detail = DeliveryNoteDetail.new
+    @delivery_note_detail = DeliveryNoteDetail.new(delivery_note_id: @delivery_note.id )
   end
 
   # GET /delivery_note_details/1/edit
@@ -27,9 +27,13 @@ class DeliveryNoteDetailsController < ApplicationController
   def create
     @delivery_note_detail = DeliveryNoteDetail.new(delivery_note_detail_params)
 
+    @delivery_note_detail[:delivery_note_id] = params[:delivery_note_id]
+
     respond_to do |format|
       if @delivery_note_detail.save
-        format.html { redirect_to @delivery_note_detail, notice: 'Delivery note detail was successfully created.' }
+        format.html { redirect_to edit_user_delivery_note_path(
+            current_user, 
+            @delivery_note_detail.delivery_note_id), notice: 'Delivery note detail was successfully created.' }
         format.json { render :show, status: :created, location: @delivery_note_detail }
       else
         format.html { render :new }
@@ -43,7 +47,9 @@ class DeliveryNoteDetailsController < ApplicationController
   def update
     respond_to do |format|
       if @delivery_note_detail.update(delivery_note_detail_params)
-        format.html { redirect_to @delivery_note_detail, notice: 'Delivery note detail was successfully updated.' }
+        format.html { redirect_to edit_user_delivery_note_path(
+            current_user, 
+            @delivery_note_detail.delivery_note_id), notice: 'Delivery note detail was successfully updated.' }
         format.json { render :show, status: :ok, location: @delivery_note_detail }
       else
         format.html { render :edit }
@@ -57,7 +63,11 @@ class DeliveryNoteDetailsController < ApplicationController
   def destroy
     @delivery_note_detail.destroy
     respond_to do |format|
-      format.html { redirect_to delivery_note_details_url, notice: 'Delivery note detail was successfully destroyed.' }
+      format.html { redirect_to edit_user_delivery_note_path(
+            current_user, 
+            @delivery_note_detail.delivery_note_id
+          ), notice: 'Delivery note detail was successfully destroyed.' 
+      }
       format.json { head :no_content }
     end
   end
@@ -70,6 +80,7 @@ class DeliveryNoteDetailsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def delivery_note_detail_params
-      params.require(:delivery_note_detail).permit(:service_id, :quantity, :price, :custom_description)
+      params.require(:delivery_note_detail)
+        .permit(:delivery_note_id, :service_id, :quantity, :price, :custom_description)
     end
 end
