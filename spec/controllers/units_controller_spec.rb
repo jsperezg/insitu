2b20484d89cd2ecbd368a6ccb353b6235f9c7886
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2015-12-04 21:46:55 +0100

    Measure units management.
    Finished controller for measure units management.

--- spec/controllers/units_controller_spec.rb
+++ spec/controllers/units_controller_spec.rb
@@ -24,37 +24,45 @@ RSpec.describe UnitsController, type: :controller do
   # Unit. As you add validations to Unit, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for :unit
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    unit = attributes_for :unit
+
+    unit[:label_short] = nil
+
+    unit
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # UnitsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  before(:each) do
+    @user = create(:user)
+    sign_in @user
+  end
+
+  after(:each) do
+    sign_out @user
+  end
 
   describe "GET #index" do
     it "assigns all units as @units" do
       unit = Unit.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:units)).to eq([unit])
+      get :index, { user_id: @user.id }
+      expect(assigns(:units)).to include(unit)
     end
   end
 
   describe "GET #show" do
     it "assigns the requested unit as @unit" do
       unit = Unit.create! valid_attributes
-      get :show, {:id => unit.to_param}, valid_session
+      get :show, { :id => unit.to_param, user_id: @user.id }
       expect(assigns(:unit)).to eq(unit)
     end
   end
 
   describe "GET #new" do
     it "assigns a new unit as @unit" do
-      get :new, {}, valid_session
+      get :new, { user_id: @user.id }
       expect(assigns(:unit)).to be_a_new(Unit)
     end
   end
@@ -62,7 +70,7 @@ RSpec.describe UnitsController, type: :controller do
   describe "GET #edit" do
     it "assigns the requested unit as @unit" do
       unit = Unit.create! valid_attributes
-      get :edit, {:id => unit.to_param}, valid_session
+      get :edit, {:id => unit.to_param, user_id: @user.id }
       expect(assigns(:unit)).to eq(unit)
     end
   end
@@ -71,30 +79,30 @@ RSpec.describe UnitsController, type: :controller do
     context "with valid params" do
       it "creates a new Unit" do
         expect {
-          post :create, {:unit => valid_attributes}, valid_session
+          post :create, {:unit => valid_attributes, user_id: @user.id }
         }.to change(Unit, :count).by(1)
       end
 
       it "assigns a newly created unit as @unit" do
-        post :create, {:unit => valid_attributes}, valid_session
+        post :create, {:unit => valid_attributes, user_id: @user.id}
         expect(assigns(:unit)).to be_a(Unit)
         expect(assigns(:unit)).to be_persisted
       end
 
       it "redirects to the created unit" do
-        post :create, {:unit => valid_attributes}, valid_session
-        expect(response).to redirect_to(Unit.last)
+        post :create, {:unit => valid_attributes, user_id: @user.id}
+        expect(response).to redirect_to(user_units_path(@user.id))
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved unit as @unit" do
-        post :create, {:unit => invalid_attributes}, valid_session
+        post :create, {:unit => invalid_attributes, user_id: @user.id}
         expect(assigns(:unit)).to be_a_new(Unit)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:unit => invalid_attributes}, valid_session
+        post :create, {:unit => invalid_attributes,user_id: @user.id}
         expect(response).to render_template("new")
       end
     end
@@ -103,39 +111,41 @@ RSpec.describe UnitsController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { label_short: 'updated label short', label_long: 'updated label long' }
       }
 
       it "updates the requested unit" do
         unit = Unit.create! valid_attributes
-        put :update, {:id => unit.to_param, :unit => new_attributes}, valid_session
+        put :update, {:id => unit.to_param, :unit => new_attributes, user_id: @user.id}
         unit.reload
-        skip("Add assertions for updated state")
+        
+        expect(unit[:label_short]).to eq(new_attributes[:label_short])
+        expect(unit[:label_long]).to eq(new_attributes[:label_long])
       end
 
       it "assigns the requested unit as @unit" do
         unit = Unit.create! valid_attributes
-        put :update, {:id => unit.to_param, :unit => valid_attributes}, valid_session
+        put :update, {:id => unit.to_param, :unit => valid_attributes, user_id: @user.id}
         expect(assigns(:unit)).to eq(unit)
       end
 
       it "redirects to the unit" do
         unit = Unit.create! valid_attributes
-        put :update, {:id => unit.to_param, :unit => valid_attributes}, valid_session
-        expect(response).to redirect_to(unit)
+        put :update, {:id => unit.to_param, :unit => valid_attributes, user_id: @user.id}
+        expect(response).to redirect_to(user_units_path(@user.id))
       end
     end
 
     context "with invalid params" do
       it "assigns the unit as @unit" do
         unit = Unit.create! valid_attributes
-        put :update, {:id => unit.to_param, :unit => invalid_attributes}, valid_session
+        put :update, {:id => unit.to_param, :unit => invalid_attributes, user_id: @user.id}
         expect(assigns(:unit)).to eq(unit)
       end
 
       it "re-renders the 'edit' template" do
         unit = Unit.create! valid_attributes
-        put :update, {:id => unit.to_param, :unit => invalid_attributes}, valid_session
+        put :update, {:id => unit.to_param, :unit => invalid_attributes, user_id: @user.id}
         expect(response).to render_template("edit")
       end
     end
@@ -145,14 +155,14 @@ RSpec.describe UnitsController, type: :controller do
     it "destroys the requested unit" do
       unit = Unit.create! valid_attributes
       expect {
-        delete :destroy, {:id => unit.to_param}, valid_session
+        delete :destroy, {:id => unit.to_param, user_id: @user.id}
       }.to change(Unit, :count).by(-1)
     end
 
     it "redirects to the units list" do
       unit = Unit.create! valid_attributes
-      delete :destroy, {:id => unit.to_param}, valid_session
-      expect(response).to redirect_to(units_url)
+      delete :destroy, {:id => unit.to_param, user_id: @user.id}
+      expect(response).to redirect_to(user_units_url(@user.id))
     end
   end
 
