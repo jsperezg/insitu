a0163c13ae7a090ed864a8f30eae4b4b54805220
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2015-12-07 19:50:26 +0100

    Invoice management.
    Added missing translations.
    Implemented controller that manages invoices.

--- app/controllers/delivery_notes_controller.rb
+++ app/controllers/delivery_notes_controller.rb
@@ -5,7 +5,7 @@ class DeliveryNotesController < ApplicationController
   # GET /delivery_notes
   # GET /delivery_notes.json
   def index
-    @delivery_notes = DeliveryNote.order(date: :desc)    
+    @delivery_notes = DeliveryNote.order(date: :desc)
   end
 
   # GET /delivery_notes/1
@@ -16,12 +16,11 @@ class DeliveryNotesController < ApplicationController
   # GET /delivery_notes/new
   def new
     @delivery_note = DeliveryNote.new( date: DateTime.now )
-
     detail = @delivery_note.delivery_note_details.build
   end
 
   # GET /delivery_notes/1/edit
-  def edit    
+  def edit
     @delivery_note_detail = DeliveryNoteDetail.new
     detail = @delivery_note.delivery_note_details.build
   end
@@ -33,7 +32,7 @@ class DeliveryNotesController < ApplicationController
 
     respond_to do |format|
       if @delivery_note.save
-        format.html { redirect_to edit_user_delivery_note_path(current_user, @delivery_note), 
+        format.html { redirect_to edit_user_delivery_note_path(current_user, @delivery_note),
                       notice: t(:successfully_created, item: t('delivery_notes.delivery_note')) }
         format.json { render :show, status: :created, location: @delivery_note }
       else
@@ -48,7 +47,7 @@ class DeliveryNotesController < ApplicationController
   def update
     respond_to do |format|
       if @delivery_note.update(delivery_note_params)
-        format.html { redirect_to edit_user_delivery_note_path(current_user, @delivery_note), 
+        format.html { redirect_to edit_user_delivery_note_path(current_user, @delivery_note),
                       notice: t(:successfully_updated, item: t('delivery_notes.delivery_note')) }
         format.json { render :show, status: :ok, location: @delivery_note }
       else
@@ -63,7 +62,7 @@ class DeliveryNotesController < ApplicationController
   def destroy
     @delivery_note.destroy
     respond_to do |format|
-      format.html { redirect_to user_delivery_notes_url(current_user), 
+      format.html { redirect_to user_delivery_notes_url(current_user),
                     notice: t(:successfully_destroyed, item: t('delivery_notes.delivery_note')) }
       format.json { head :no_content }
     end
@@ -78,8 +77,8 @@ class DeliveryNotesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def delivery_note_params
       params.require(:delivery_note).permit(
-        :customer_id, 
-        :date, 
+        :customer_id,
+        :date,
         delivery_note_details_attributes: [
           :id, :delivery_note_id, :service_id, :quantity, :price, :custom_description, :_destroy
         ]
--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,7 +5,7 @@ class InvoicesController < ApplicationController
   # GET /invoices
   # GET /invoices.json
   def index
-    @invoices = Invoice.all
+    @invoices = Invoice.order(date: :desc)
   end
 
   # GET /invoices/1
@@ -16,10 +16,13 @@ class InvoicesController < ApplicationController
   # GET /invoices/new
   def new
     @invoice = Invoice.new
+    detail = @invoice.invoice_details.build
   end
 
   # GET /invoices/1/edit
   def edit
+    @invoice_detail = InvoiceDetail.new
+    detail = @invoice.invoice_details.build
   end
 
   # POST /invoices
@@ -29,7 +32,10 @@ class InvoicesController < ApplicationController
 
     respond_to do |format|
       if @invoice.save
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully created.' }
+        format.html {
+          redirect_to user_edit_invoice_path(current_user, @invoice),
+          t(:successfully_created, item: t('invoices.invoice'))
+        }
         format.json { render :show, status: :created, location: @invoice }
       else
         format.html { render :new }
@@ -43,7 +49,10 @@ class InvoicesController < ApplicationController
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully updated.' }
+        format.html {
+          redirect_to user_edit_invoice_path(current_user, @invoice),
+          notice: t(:successfully_updated, item: t('invoices.invoice'))
+        }
         format.json { render :show, status: :ok, location: @invoice }
       else
         format.html { render :edit }
@@ -57,7 +66,10 @@ class InvoicesController < ApplicationController
   def destroy
     @invoice.destroy
     respond_to do |format|
-      format.html { redirect_to invoices_url, notice: 'Invoice was successfully destroyed.' }
+      format.html {
+        redirect_to user_invoices_url(current_user),
+        notice: t(:successfully_destroyed, item: t('invoices.invoice'))
+      }
       format.json { head :no_content }
     end
   end
@@ -70,6 +82,16 @@ class InvoicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def invoice_params
-      params.require(:invoice).permit(:number, :date, :payment_method_id, :customer_id, :issue_date, :payment_date, :paid_on)
+      params.require(:invoice).permit(
+        :date,
+        :payment_method_id,
+        :customer_id,
+        :payment_date,
+        :paid_on,
+        invoice_details_attributes: [
+          :id, :invoice_id, :service_id, :vat_rate, :quantity, :price,
+          :discount, :description, :_destroy
+        ]
+      )
     end
 end
