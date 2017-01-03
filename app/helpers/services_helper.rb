7bfa4d3fa469142b408c980f5441e7963b371978
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-01-03 13:55:17 +0100

    Printing improvements for invoices. Fixed bugs in tasks & project views. Removal of unused code.

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,17 +1,4 @@
 module ServicesHelper
-  def services_select(form, method)
-    form.collection_select method,
-      Service.order(description: :asc), :id, :description,
-      {
-          disabled: lambda { |service| !service.active },
-          prompt: true
-      },
-      {
-          class: 'form-control input-sm',
-          onchange: "INSITU.services.loadService('#{ user_services_path(current_user)}', this, '#{ form.object_name }')"
-      }
-  end
-
   def service_options
     options_for_select(
         Service.order(description: :asc).map { |service| [service.description, service.id ] },
