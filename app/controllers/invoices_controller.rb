50362d2dee771b4d14fb205767ef33958c39fe70
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-01-05 08:47:17 +0100

    Improvements for Date Inputs

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -10,7 +10,7 @@ class InvoicesController < SecuredController
       Invoice,
       params[:filterrific],
       default_filter_params: {
-          with_date_ge: I18n.l(Date.today.beginning_of_year),
+          with_date_ge: Date.today.beginning_of_year.strftime('%Y-%m-%d'),
           sorted_by: 'date_desc'
       }
     ) or return
