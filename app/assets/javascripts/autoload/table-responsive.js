13bbaad89b9fdc89ba1424955bb2d09e35f2ab6f
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-01-26 07:07:42 +0100

    New context menu in invoces list.

---
+++ app/assets/javascripts/autoload/table-responsive.js
@@ -0,0 +1,7 @@
+$(document).on("page:change", function () {
+  $('body').on('show.bs.dropdown', '.table-responsive', function () {
+    $(this).css("overflow", "visible");
+  }).on('hide.bs.dropdown', '.table-responsive', function () {
+    $(this).css("overflow", "auto");
+  });
+});
