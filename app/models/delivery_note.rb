507f8d4494acaad1eaed3451bf8fc4e514beda86
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-02-04 08:14:16 +0100

    Fixed issues with missing sort criteria  in filters.

--- app/models/delivery_note.rb
+++ app/models/delivery_note.rb
@@ -67,7 +67,7 @@ class DeliveryNote < ActiveRecord::Base
     parts = sort_by.split('_')
 
     if parts.empty?
-      order(date:  :asc)
+      order(date:  :desc)
     elsif parts[0] == 'customer'
       joins(:customer).order("customers.name #{ parts[1] }")
     else
