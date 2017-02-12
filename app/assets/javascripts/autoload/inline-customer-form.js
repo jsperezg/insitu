9a87ad7aa26f8367272f2f711580a453be99e79b
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-02-12 10:18:39 +0100

    InLine customer creation finished.

--- app/assets/javascripts/autoload/inline-customer-form.js
+++ app/assets/javascripts/autoload/inline-customer-form.js
@@ -2,6 +2,18 @@ $(document).on('turbolinks:load', function() {
   var inlineCustomerForm = $('#new-customer-inline');
 
   if (inlineCustomerForm.length) {
+    inlineCustomerForm.on('show.bs.modal', function () {
+      inlineCustomerForm.find('input[type=text]').each(function () {
+        $(this).prop('title', '');
+        $(this).parent().removeClass('has-error');
+        $(this).val('');
+      });
+    });
+
+    inlineCustomerForm.on('shown.bs.modal', function () {
+      $('#customer_tax_id').focus();
+    });
+
     inlineCustomerForm.on('ajax:success', function (e, data, status, xhr) {
       var customerSelector = $('.customer-selector');
 
@@ -12,8 +24,18 @@ $(document).on('turbolinks:load', function() {
 
       $('#create-customer-modal').modal('hide');
     }).on('ajax:error', function (e, xhr, status, error) {
-      //TODO Deal with errors.
-      console.log(e, xhr, status, error);
+      var attribute, input;
+
+      for (attribute in xhr.responseJSON) {
+        if (xhr.responseJSON.hasOwnProperty(attribute)) {
+          input = $('#customer_' + attribute);
+
+          if (input.length) {
+            input.prop('title', xhr.responseJSON[attribute][0]);
+            input.parent().addClass('has-error');
+          }
+        }
+      }
     });
   }
 });
--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,5 +1,4 @@
 module CustomersHelper
-  #TODO refactor this function to simplify how it works.
   def customer_field(form, method, options)
     content = []
 
@@ -22,7 +21,52 @@ module CustomersHelper
     hidden_class = ''
     hidden_class = 'filterrific-periodically-observed' if options[:filterrific]
 
-    # value for approximate search
+    content_tag(:div, class: options[:class], url_source: user_customers_path(current_user, format: :json)) do
+      content << customer_label_tag(form, method, options)
+
+      if create_customer_button
+        content << content_tag(:div, class: 'input-group') do
+          button_group = []
+          button_group << find_customer_tag(form, method, options)
+          button_group << create_customer_tag
+          raw(button_group.join(''))
+        end
+      else
+        content << find_customer_tag(form, method, options)
+      end
+
+      content << form.hidden_field(method, class: hidden_class)
+      raw(content.join(''))
+    end
+  end
+
+  def create_customer_tag
+    content_tag(:div, class: 'input-group-btn') do
+      button_tag t(:new), class: 'btn btn-default btn-sm',
+                 type: 'button',
+                 data: {toggle: 'modal', target: '#create-customer-modal'}
+    end
+  end
+
+  def customer_label_tag(form, method, options)
+    if options.key? :label
+      form.label(method, options[:label], class: 'control-label', for: "#{ form.object_name }_#{ method }_finder")
+    else
+      form.label(method, class: 'control-label', for: "#{ form.object_name }_#{ method }_finder")
+    end
+  end
+
+  def find_customer_tag(form, method, options)
+    value = customer_field_value(form)
+
+    text_field_tag("#{ form.object_name }_#{ method }_finder",
+                   value,
+                   class: options[:input_class],
+                   placeholder: I18n.t('customers.select_customer_placeholder'),
+                   required: options[:required])
+  end
+
+  def customer_field_value(form)
     value = ''
 
     if !form.object.nil? && form.object.respond_to?(:customer)
@@ -30,44 +74,7 @@ module CustomersHelper
       value = customer.send('name') unless customer.nil?
     end
 
-    content_tag(:div, class: options[:class], url_source: user_customers_path(current_user, format: :json)) do
-      if options.key? :label
-        content << form.label(method, options[:label], class: 'control-label', for: "#{ form.object_name }_#{ method }_finder")
-      else
-        content << form.label(method, class: 'control-label', for: "#{ form.object_name }_#{ method }_finder")
-      end
-
-
-      if create_customer_button
-        content << content_tag(:div, class: 'input-group') do
-          button_group = []
-
-          button_group << text_field_tag("#{ form.object_name }_#{ method }_finder",
-                                  value,
-                                  class: options[:input_class],
-                                  placeholder: I18n.t('customers.select_customer_placeholder'),
-                                  required: options[:required])
-
-
-            button_group << content_tag(:div, class: 'input-group-btn') do
-              button_tag t(:new), class: 'btn btn-default btn-sm',
-                         type: 'button',
-                         data: { toggle: 'modal', target: '#create-customer-modal'}
-          end
-
-          raw(button_group.join(''))
-        end
-      else
-        content << text_field_tag("#{ form.object_name }_#{ method }_finder",
-                       value,
-                       class: options[:input_class],
-                       placeholder: I18n.t('customers.select_customer_placeholder'),
-                       required: options[:required])
-      end
-
-      content << form.hidden_field(method, class: hidden_class)
-      raw(content.join(''))
-    end
+    value
   end
 end
 
